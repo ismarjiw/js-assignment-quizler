1bc2274569d24113c944a9825118d9e2f30a832e
Ismarji Workman <ismarjiw@protonmail.com>
2024-02-06 21:10:24 -0500

    initial commit

--- src/lib.js
+++ src/lib.js
@@ -1,16 +1,71 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
-}
+export const chooseRandom = (array = [], numItems) => {
+  if (array.length <= 1) {
+    return array.slice() 
+  }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
-}
+  if (numItems < 1 || numItems > array.length) {
+    numItems = Math.floor(Math.random() * (array.length)) + 1
+  }
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
-}
+  const shuffledArray = array.slice().sort(() => Math.random() - 0.5)
+
+  return shuffledArray.slice(0, numItems)
+};
+
+
+export const createPrompt = ({ numQuestions = 1, numChoices = 2 } = {}) => {
+
+  numQuestions = Math.max(1, numQuestions)
+  numChoices = Math.max(2, numChoices)
+
+  const prompts = [];
+
+  for (let q = 1; q <= numQuestions; q++) {
+    const question = {
+      type: 'input',
+      name: `question-${q}`,
+      message: `Enter question ${q}`
+    }
+    prompts.push(question)
+
+    for (let c = 1; c <= numChoices; c++) {
+      const choice = {
+        type: 'input',
+        name: `question-${q}-choice-${c}`,
+        message: `Enter answer choice ${c} for question ${q}`
+      };
+      prompts.push(choice)
+    }
+  }
+
+  return prompts
+};
+
+
+export const createQuestions = (obj = {}) => {
+  const questions = []
+
+  for (let key in obj) {
+      const questionName = key
+      const questionData = obj[key]
+
+      const { question, choices } = questionData
+
+      const questionObject = {
+        type: 'list',
+        name: questionName,
+        message: question,
+        choices: choices
+      }
+
+      questions.push(questionObject)
+  }
+
+  return questions
+};
+
 
 export const readFile = path =>
   new Promise((resolve, reject) => {
